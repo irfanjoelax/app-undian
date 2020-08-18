5f81605f17c0bc8dca41ea154c89c1e86cdc3cc0
irfanjoelax <irfanthejoelax@gmail.com>
2020-08-18 23:10:22 +0800

    v.1.9 - add paket phpSpreadsheet

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInitf5e876a42bd60d5ff403e25c1614a5cb
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitf5e876a42bd60d5ff403e25c1614a5cb::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequiref5e876a42bd60d5ff403e25c1614a5cb($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequiref5e876a42bd60d5ff403e25c1614a5cb($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
